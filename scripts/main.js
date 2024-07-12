d7019111df2315d09efa9606556ad0a94a72e83b
Hank <hank.la.boon@gmail.com>
2024-07-12 09:54:29 +0200

    replaced setAllUnits

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ const outputAreaField = document.getElementById("outputAreaField");
 const resetButton = document.getElementById("resetButton");
 let inputArmyString = "";
 let inputArmyLinebreakArray;
-let outputArmyArray = [];
+let outputArmyObject = [];
 let armyArrayIndex = -1;   // anders lösen?
 let armyTitle = "";
 let firstEmptyLineIndex = "";
@@ -33,7 +33,8 @@ function getAndSetFaction() {
 function getAndSetDetachment() {
     for (const detachment of tauEmpire.detachments) {
         if (inputArmyString.includes(detachment)) {
-            outputAreaField.textContent += "*" + detachment + "*" + "\n"
+            outputAreaField.textContent += "*" + detachment + "*" + "\n";
+            console.log(outputAreaField.textContent)
         }
     }
 }
@@ -51,13 +52,12 @@ function getUnit(string, unitName, singleModelNames, minSize, weapons, enhanceme
         const searchAreaLinebreakArray = searchArea.split((/\r?\n|\r|\n/g));
         for (const line of searchAreaLinebreakArray) {
             if (line.includes(unitName) && line.includes("oints")) {
-                armyArrayIndex += 1;
-                outputArmyArray.push({
+                armyArrayIndex++;
+                outputArmyObject.push({
                     name: unitName,
                     singleModelNames: singleModelNames,
                     numberOfModels: 0,
                     minSize: minSize,
-                    points: "",
                     equipedWeapons: [],
                     warlord: [],
                     enhancement: [],
@@ -75,7 +75,7 @@ function getUnit(string, unitName, singleModelNames, minSize, weapons, enhanceme
                 if (line.includes(singleModelName) && line.includes("•")) {
                     const trimmedLine = line.trim();
                     let onlyNumber = Number(trimmedLine.substring(2, (trimmedLine.length - singleModelName.length - 2)));
-                    outputArmyArray[armyArrayIndex].numberOfModels += onlyNumber;
+                    outputArmyObject[armyArrayIndex].numberOfModels += onlyNumber;
                 }
 
             }
@@ -85,14 +85,14 @@ function getUnit(string, unitName, singleModelNames, minSize, weapons, enhanceme
             if (line.includes(unitName) && line.includes("oints")) {
                 const trimmedLine = line.trim();
                 const onlyPoints = trimmedLine.substring((unitName.length + 2), (trimmedLine.length - 8));
-                outputArmyArray[armyArrayIndex].points = onlyPoints;
+                outputArmyObject[armyArrayIndex].points = onlyPoints;
             }
         }
 
         for (const weapon of weapons) {
             if (searchArea.includes(weapon.name) && weapon.display === "true") {
                 const searchAreaLinebreakArray = searchArea.split((/\r?\n|\r|\n/g));
-                outputArmyArray[armyArrayIndex].equipedWeapons.push({
+                outputArmyObject[armyArrayIndex].equipedWeapons.push({
                     name: weapon.alias,
                     count: 0
                 })
@@ -100,7 +100,7 @@ function getUnit(string, unitName, singleModelNames, minSize, weapons, enhanceme
                     if (line.includes(weapon.name)) {
                         for (i = 1; i < 10; i++) {
                             if (line.includes(i)) {
-                                outputArmyArray[armyArrayIndex].equipedWeapons[outputArmyArray[armyArrayIndex].equipedWeapons.length - 1].count += i;  // kann ich hier mit this abkürzen?
+                                outputArmyObject[armyArrayIndex].equipedWeapons[outputArmyObject[armyArrayIndex].equipedWeapons.length - 1].count += i;  // kann ich hier mit this abkürzen?
                             }
                         }
                     }
@@ -109,13 +109,13 @@ function getUnit(string, unitName, singleModelNames, minSize, weapons, enhanceme
         }
 
         if (searchArea.includes("Warlord")) {
-            outputArmyArray[armyArrayIndex].warlord.push("Warlord");
+            outputArmyObject[armyArrayIndex].warlord.push("Warlord");
         }
 
         if (searchArea.includes("Enhancement")) {
             for (const enhancement of enhancements) {
                 if (searchArea.includes(enhancement)) {
-                    outputArmyArray[armyArrayIndex].enhancement.push(enhancement);
+                    outputArmyObject[armyArrayIndex].enhancement.push(enhancement);
                 }
             }
         }
@@ -128,82 +128,111 @@ function getAllUnits() {
         getUnit(inputArmyString, unit.name, unit.singleModelNames, unit.minSize, unit.weapons, unit.enhancements);
     }
 }
-// in cases ändern! struktur ändern, so dass text content erst am ende gefüllt wird.
-function setAllUnits() {
-    for (const item of outputArmyArray) {
+let allUnitsArray = [];
+function setAllUnitsToArray() {
+    let index = -1
+    for (const item of outputArmyObject) {
+        index++;
+        allUnitsArray.push(item.name)
         if (item.numberOfModels !== 0) {
-            if (item.equipedWeapons.length !== 0 && item.warlord.length === 0) {
-                let rawWeaponString = "";
-                for (const weapon of item.equipedWeapons) {
-                    rawWeaponString += `${weapon.count}x ${weapon.name}, `;
-                }
-                const weaponString = rawWeaponString.slice(rawWeaponString[1], rawWeaponString.length - 2);  //warum ist index 1 nicht index0?
-                outputAreaField.textContent += `- ${item.name} [${item.numberOfModels}x] [${weaponString}] ${item.points}\n`
-            }
-            else if (item.equipedWeapons.length !== 0 && item.warlord.length !== 0) {
-                let rawWeaponString = "";
-                for (const weapon of item.equipedWeapons) {
-                    rawWeaponString += `${weapon.count}x ${weapon.name}, `;
-                }
-                const weaponString = rawWeaponString.slice(rawWeaponString[1], rawWeaponString.length - 2);
-                outputAreaField.textContent += `- ${item.name} [${item.numberOfModels}x] [${weaponString}] [${item.warlord}] ${item.points}\n`
-            }
-            else if (item.equipedWeapons.length === 0 && item.warlord.length !== 0) {
-                outputAreaField.textContent += `- ${item.name} [${item.numberOfModels}x] [${item.warlord}] ${item.points}\n`
-            }
-            else {
-                outputAreaField.textContent += `- ${item.name} [${item.numberOfModels}x] ${item.points}\n`
-            }
+            allUnitsArray[index] += ` [${item.numberOfModels}]`
         }
-
-        if (item.numberOfModels === 0) {
-            if (item.equipedWeapons.length !== 0 && item.warlord.length === 0 && item.enhancement.length === 0) {
-                let rawWeaponString = "";
-                for (const weapon of item.equipedWeapons) {
-                    rawWeaponString += `${weapon.count}x ${weapon.name}, `;
-                }
-                const weaponString = rawWeaponString.slice(rawWeaponString[1], rawWeaponString.length - 2);  //warum ist index 1 nicht index0?
-                outputAreaField.textContent += `- ${item.name} [${weaponString}] ${item.points}\n`
-            }
-            else if (item.equipedWeapons.length !== 0 && item.warlord.length === 0 && item.enhancement !== 0) {
-                let rawWeaponString = "";
-                for (const weapon of item.equipedWeapons) {
-                    rawWeaponString += `${weapon.count}x ${weapon.name}, `;
-                }
-                const weaponString = rawWeaponString.slice(rawWeaponString[1], rawWeaponString.length - 2);  //warum ist index 1 nicht index0?
-                outputAreaField.textContent += `- ${item.name} [${item.enhancement[0]}] [${weaponString}] ${item.points}\n`
-            }
-            else if (item.equipedWeapons.length !== 0 && item.warlord.length !== 0 && item.enhancement.length === 0) {
-                let rawWeaponString = "";
-                for (const weapon of item.equipedWeapons) {
-                    rawWeaponString += `${weapon.count}x ${weapon.name}, `;
-                }
-                const weaponString = rawWeaponString.slice(rawWeaponString[1], rawWeaponString.length - 2);
-                outputAreaField.textContent += `- ${item.name} [${weaponString}] [${item.warlord}] ${item.points}\n`
-            }
-            else if (item.equipedWeapons.length !== 0 && item.warlord.length !== 0 && item.enhancement.length !== 0) {
-                let rawWeaponString = "";
-                for (const weapon of item.equipedWeapons) {
-                    rawWeaponString += `${weapon.count}x ${weapon.name}, `;
-                }
-                const weaponString = rawWeaponString.slice(rawWeaponString[1], rawWeaponString.length - 2);
-                outputAreaField.textContent += `- ${item.name} [${item.enhancement}] [${weaponString}] [${item.warlord}] ${item.points}\n`
-            }
-            else if (item.equipedWeapons.length === 0 && item.warlord.length !== 0) {
-                outputAreaField.textContent += `- ${item.name} [${item.warlord}] ${item.points}\n`
-            }
-            else if (item.equipedWeapons.length === 0 && item.warlord.length !== 0 && item.enhancement.length !== 0) {
-                outputAreaField.textContent += `- ${item.name} [${item.enhancement[0]}] [${item.warlord}] ${item.points}\n`
-            }
-            else if (item.equipedWeapons.length === 0 && item.warlord.length === 0 && item.enhancement.length !== 0) {
-                outputAreaField.textContent += `- ${item.name} [${item.enhancement[0]}] ${item.points}\n`
-            }
-            else {
-                outputAreaField.textContent += `- ${item.name} ${item.points}\n`
-            }
+        if (item.warlord.length !== 0) {
+            allUnitsArray[index] += ` [Warlord]`
         }
+        if (item.equipedWeapons.length !== 0) {
+            let rawWeaponString = "";
+            for (const weapon of item.equipedWeapons) {
+                rawWeaponString += `${weapon.count}x ${weapon.name}, `;
+            }
+            const weaponString = rawWeaponString.slice(rawWeaponString[1], rawWeaponString.length - 2)
+            allUnitsArray[index] += ` [${weaponString}]`
+        }
+        allUnitsArray[index] += ` ${item.points}`
     }
 }
+function setAllUnits(){
+   for(const item of allUnitsArray){
+    outputAreaField.textContent += `- ${item}\n`   
+   } 
+}
+
+// in cases ändern! struktur ändern, so dass text content erst am ende gefüllt wird.
+// function setAllUnits() {
+//     for (const item of outputArmyObject) {
+//         if (item.numberOfModels !== 0) {
+//             if (item.equipedWeapons.length !== 0 && item.warlord.length === 0) {
+//                 let rawWeaponString = "";
+//                 for (const weapon of item.equipedWeapons) {
+//                     rawWeaponString += `${weapon.count}x ${weapon.name}, `;
+//                 }
+//                 const weaponString = rawWeaponString.slice(rawWeaponString[1], rawWeaponString.length - 2);  //warum ist index 1 nicht index0?
+//                 outputAreaField.textContent += `- ${item.name} [${item.numberOfModels}x] [${weaponString}] ${item.points}\n`
+//             }
+//             else if (item.equipedWeapons.length !== 0 && item.warlord.length !== 0) {
+//                 let rawWeaponString = "";
+//                 for (const weapon of item.equipedWeapons) {
+//                     rawWeaponString += `${weapon.count}x ${weapon.name}, `;
+//                 }
+//                 const weaponString = rawWeaponString.slice(rawWeaponString[1], rawWeaponString.length - 2);
+//                 outputAreaField.textContent += `- ${item.name} [${item.numberOfModels}x] [${weaponString}] [${item.warlord}] ${item.points}\n`
+//             }
+//             else if (item.equipedWeapons.length === 0 && item.warlord.length !== 0) {
+//                 outputAreaField.textContent += `- ${item.name} [${item.numberOfModels}x] [${item.warlord}] ${item.points}\n`
+//             }
+//             else {
+//                 outputAreaField.textContent += `- ${item.name} [${item.numberOfModels}x] ${item.points}\n`
+//             }
+//         }
+
+//         if (item.numberOfModels === 0) {
+//             if (item.equipedWeapons.length !== 0 && item.warlord.length === 0 && item.enhancement.length === 0) {
+//                 let rawWeaponString = "";
+//                 for (const weapon of item.equipedWeapons) {
+//                     rawWeaponString += `${weapon.count}x ${weapon.name}, `;
+//                 }
+//                 const weaponString = rawWeaponString.slice(rawWeaponString[1], rawWeaponString.length - 2);  //warum ist index 1 nicht index0?
+//                 outputAreaField.textContent += `- ${item.name} [${weaponString}] ${item.points}\n`
+//             }
+//             else if (item.equipedWeapons.length !== 0 && item.warlord.length === 0 && item.enhancement !== 0) {
+//                 let rawWeaponString = "";
+//                 for (const weapon of item.equipedWeapons) {
+//                     rawWeaponString += `${weapon.count}x ${weapon.name}, `;
+//                 }
+//                 const weaponString = rawWeaponString.slice(rawWeaponString[1], rawWeaponString.length - 2);  //warum ist index 1 nicht index0?
+//                 outputAreaField.textContent += `- ${item.name} [${item.enhancement[0]}] [${weaponString}] ${item.points}\n`
+//             }
+//             else if (item.equipedWeapons.length !== 0 && item.warlord.length !== 0 && item.enhancement.length === 0) {
+//                 let rawWeaponString = "";
+//                 for (const weapon of item.equipedWeapons) {
+//                     rawWeaponString += `${weapon.count}x ${weapon.name}, `;
+//                 }
+//                 const weaponString = rawWeaponString.slice(rawWeaponString[1], rawWeaponString.length - 2);
+//                 outputAreaField.textContent += `- ${item.name} [${weaponString}] [${item.warlord}] ${item.points}\n`
+//             }
+//             else if (item.equipedWeapons.length !== 0 && item.warlord.length !== 0 && item.enhancement.length !== 0) {
+//                 let rawWeaponString = "";
+//                 for (const weapon of item.equipedWeapons) {
+//                     rawWeaponString += `${weapon.count}x ${weapon.name}, `;
+//                 }
+//                 const weaponString = rawWeaponString.slice(rawWeaponString[1], rawWeaponString.length - 2);
+//                 outputAreaField.textContent += `- ${item.name} [${item.enhancement}] [${weaponString}] [${item.warlord}] ${item.points}\n`
+//             }
+//             else if (item.equipedWeapons.length === 0 && item.warlord.length !== 0) {
+//                 outputAreaField.textContent += `- ${item.name} [${item.warlord}] ${item.points}\n`
+//             }
+//             else if (item.equipedWeapons.length === 0 && item.warlord.length !== 0 && item.enhancement.length !== 0) {
+//                 outputAreaField.textContent += `- ${item.name} [${item.enhancement[0]}] [${item.warlord}] ${item.points}\n`
+//             }
+//             else if (item.equipedWeapons.length === 0 && item.warlord.length === 0 && item.enhancement.length !== 0) {
+//                 outputAreaField.textContent += `- ${item.name} [${item.enhancement[0]}] ${item.points}\n`
+//             }
+//             else {
+//                 outputAreaField.textContent += `- ${item.name} ${item.points}\n`
+//             }
+//         }
+//     }
+// }
 
 
 function compressList(event) {
@@ -216,6 +245,8 @@ function compressList(event) {
     getAndSetFaction();
     getAndSetDetachment();
     getAllUnits();
+    // setAllUnits();
+    setAllUnitsToArray();
     setAllUnits();
 }
 
@@ -229,4 +260,5 @@ inputForm.addEventListener("submit", compressList);
 resultForm.addEventListener("submit", copyToClipboard);
 resetButton.addEventListener("click", () => { location.reload() });
 
-console.log(outputArmyArray);
+console.log(outputArmyObject);
+console.log(allUnitsArray);
